ee684047139a19dd16177575dffdf7564d77b5aa
Serhat Kaya <l.serhat.kaya.l@gmail.com>
2021-03-07 23:44:53 +0300

    Created the basic message board with a send form

---
+++ routes/index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Mini message board', messages: messages });
+});
+
+/* GET send message page */
+router.get('/new', function(req, res, next) {
+  res.render('form', { title: 'Send message' });
+});
+
+/* POST message */
+router.post('/new', function(req, res, next) {
+  messages.push({ text: req.body.message, user: req.body.author, added: new Date() });
+  res.redirect('/');
+})
+
+module.exports = router;
